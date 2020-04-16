bd444c8b2a2a95bbd8309287df7babb6bcfc0f40
luongthanhtung9 <tunglt2129@gmail.com>
2020-04-16 17:35:30 +0700

    Update MainActivity.java

--- app/src/main/java/com/example/newsfeeds/activity/MainActivity.java
+++ app/src/main/java/com/example/newsfeeds/activity/MainActivity.java
@@ -1,5 +1,5 @@
 package com.example.newsfeeds.activity;
-
+//thử
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
 import androidx.core.app.ActivityOptionsCompat;
